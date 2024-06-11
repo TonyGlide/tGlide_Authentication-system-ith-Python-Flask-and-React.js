b5a9b986e76afdacca927e6ffe43367c7af4c4d5
TonyGlide <162181740+TonyGlide@users.noreply.github.com>
2024-06-11 01:17:13 +0000

    sd

--- src/front/js/pages/login.js
+++ src/front/js/pages/login.js
@@ -5,6 +5,13 @@ import "../../styles/home.css";
 export const Login = () => {
 	const { store, actions } = useContext(Context);
 
+	// 1 create 2 useStates, one for email the other for password
+	// 3 create a funtion called handleClick that will include the fetch with opstions that includes
+	// the email and password
+
+	// 2 make the inputs controlled <inputs>
+	// also, make the button execute the handleClick
+
 	return (
 		<div className="text-center mt-5">
 			<h1>Login</h1>
